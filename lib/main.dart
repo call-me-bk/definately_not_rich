228ca3e6262215351d00cf5c6241529a4a8e7df5
B K Karthik <bkkarthik@pesu.pes.edu>
2021-01-01 09:38:53 +0530

    Added Scaffold with an AppBar under under it.
    Lesson 2. following https://youtu.be/l9nl-WkABIo as a reference :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+//Entry point
 void main() {
   runApp(MaterialApp(
-    home: Center(child: Text('Happy new year 2021 :)'),),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Definitely not rich :)"),
+      ),
+    ),
   ));
 }
-
